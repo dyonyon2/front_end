c559d8c159361da6a6602f9ae8cea7604a7e8d1b
jychoi9712 <jychoi9712@naver.com>
2022-04-19 21:52:30 +0900

    part9 ch6 clear

--- part9/ch4/movie/jest.config.js
+++ part9/ch4/movie/jest.config.js
@@ -16,8 +16,12 @@ module.exports = {
     // 일치하는 경로에서는 모듈을 가져오지 않는다.
     // '<rootDir> 토큰을 사용해 루트 경로를 참조한다.
     modulePathIgnorePatterns: [
-        '<rootDir>/part9/ch4/movie/node_modules',
-        '<rootDir>/part9/ch4/movie/dist'
+        // '<rootDir>/part9/ch4/movie/node_modules',
+        // '<rootDir>/part9/ch4/movie/dist',
+        // '<rootDir>/part9/ch4/movie/cypress',
+        '<rootDir>/node_modules',
+        '<rootDir>/dist',
+        '<rootDir>/cypress'
     ],
 
     // jsdom 환경에 대한 URL을 설정한다.
