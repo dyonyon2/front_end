bfdf93e38ef90c5dafa8ffd76b559148c82c67dc
jychoi9712 <jychoi9712@naver.com>
2022-04-24 02:49:27 +0900

    part10 Clear!!

--- part10/ch7/react-router-example/src/components/NavLinks.jsx
+++ part10/ch7/react-router-example/src/components/NavLinks.jsx
@@ -3,45 +3,48 @@ import { NavLink } from "react-router-dom";
 const activeStyle = { color: "green" };
 
 export default function NavLinks() {
-  return (
-    <ul>
-      <li>
-        <NavLink to="/" exact activeStyle={activeStyle}>
-          Home
-        </NavLink>
-      </li>
-      <li>
-        <NavLink to="/profile" exact activeStyle={activeStyle}>
-          Profile
-        </NavLink>
-      </li>
-      <li>
-        <NavLink to="/profile/1" activeStyle={activeStyle}>
-          Profile 1
-        </NavLink>
-      </li>
-      <li>
-        <NavLink
-          to="/about"
-          activeStyle={activeStyle}
-          isActive={(match, location) => {
-            return match !== null && location.search === "";
-          }}
-        >
-          About
-        </NavLink>
-      </li>
-      <li>
-        <NavLink
-          to="/about?name=dyonyon"
-          activeStyle={activeStyle}
-          isActive={(match, location) => {
-            return match !== null && location.search === "?name=dyonyon";
-          }}
-        >
-          About?name=dyonyon
-        </NavLink>
-      </li>
-    </ul>
-  );
+    return (
+        <ul>
+            <li>
+                <NavLink to="/" exact activeStyle={activeStyle}>
+                    Home
+                </NavLink>
+            </li>
+            <li>
+                <NavLink to="/profile" exact activeStyle={activeStyle}>
+                    Profile
+                </NavLink>
+            </li>
+            <li>
+                <NavLink to="/profile/1" activeStyle={activeStyle}>
+                    Profile 1
+                </NavLink>
+            </li>
+            <li>
+                <NavLink
+                    to="/about"
+                    activeStyle={activeStyle}
+                    isActive={(match, location) => {
+                        return match !== null && location.search === "";
+                    }}
+                >
+                    About
+                </NavLink>
+            </li>
+            <li>
+                <NavLink
+                    to="/about?name=dyonyon"
+                    activeStyle={activeStyle}
+                    isActive={(match, location) => {
+                        return (
+                            match !== null &&
+                            location.search === "?name=dyonyon"
+                        );
+                    }}
+                >
+                    About?name=dyonyon
+                </NavLink>
+            </li>
+        </ul>
+    );
 }
--- part10/ch7/react-router-example/src/pages/About.jsx
+++ part10/ch7/react-router-example/src/pages/About.jsx
@@ -8,22 +8,22 @@ export default function About(props) {
 import queryString from "query-string";
 
 export default function About(props) {
-  // console.log(props);
+    // console.log(props);
 
-  const searchParams = props.location.search;
-  // console.log(searchParams);
+    const searchParams = props.location.search;
+    // console.log(searchParams);
 
-  // URLSearchParams 방식! => 두가지 단점 존재
-  // const obj = new URLSearchParams(searchParams);
-  // console.log(obj.get("name"));
+    // URLSearchParams 방식! => 두가지 단점 존재
+    // const obj = new URLSearchParams(searchParams);
+    // console.log(obj.get("name"));
 
-  const query = queryString.parse(searchParams);
-  // console.log(query);
+    const query = queryString.parse(searchParams);
+    // console.log(query);
 
-  return (
-    <div>
-      <h2>About 페이지입니다.</h2>
-      {query.name && <p>name 은 {query.name} 입니다. </p>}
-    </div>
-  );
+    return (
+        <div>
+            <h2>About 페이지입니다.</h2>
+            {query.name && <p>name 은 {query.name} 입니다. </p>}
+        </div>
+    );
 }
